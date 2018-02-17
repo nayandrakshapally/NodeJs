911f7b5958363fe4b50f95ae0393c684c5fe552e
nayan <nayan.drakshapally@gmail.com>
2018-02-17 21:47:28 +0530

    Node Callbacks and Error Handling

--- node-examples/index.js
+++ node-examples/index.js
@@ -4,17 +4,32 @@
 // };
 var rect = require("./rectangle");
 
-function solveRect(l,b){
-console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-if(l<=0 || b<=0){
-    console.log("values of l and b must be greater than zero");
-}else{
-    console.log("value of perimeter", + rect.perimeter(l,b));
-    console.log("value of area", + rect.area(l,b));
-}
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 
-}
+// if(l<=0 || b<=0){
+//     console.log("values of l and b must be greater than zero");
+// }else{
+//     console.log("value of perimeter", + rect.perimeter(l,b));
+//     console.log("value of area", + rect.area(l,b));
+// }
+
+
 solveRect(2,4);
 solveRect(3,5);
 solveRect(0,5);
