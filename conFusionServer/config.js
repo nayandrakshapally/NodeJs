fa2abc50c1a42802a8a999354576dd5cfe3ca09a
nayan <nayan.drakshapally@gmail.com>
2018-03-21 23:18:58 +0530

    facebook auth

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '217368005508660',
+        clientSecret: '078f7f2dc7a485d9724083aba1d9698d'
+    }
 }
